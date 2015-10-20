e5342f27db3d3efc8c75581eeeca5c6dde4fbe60
Eric Richards <ericrrichards@gmail.com>
2015-10-19 23:09:09 -0400

    integrate with vlc

--- ChannelListings/Modules/MainModule.cs
+++ ChannelListings/Modules/MainModule.cs
@@ -5,7 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ChannelListings.Modules {
+    using System.Net;
+    using System.Xml.Linq;
+
     using Nancy;
+    using Nancy.Responses;
 
     using tvListings;
 
@@ -23,18 +27,60 @@ namespace ChannelListings.Modules {
             661,398,410,427,432,423,435,12014,3486,425, 433,3460,26,1232,1254,9426, 3389,4579,44425,30617,43574,14833
 
         };
+        private static List<VlcPlaylistItem> _playlist;
+
         public MainModule() {
             Get[""]=Get["/"]=Get["/Index"]=Get["index"] = Index;
             Get["/All"] = All;
+            Get["/play/{id}"] = Play;
         }
 
         private dynamic Index(dynamic parameters) {
 
-            var schedule = new RoviApi(_apiKey).GetSchedule(_vlcSourceIds, 180);
+            _playlist = GetPlaylist();
 
+            var schedule = new RoviApi(_apiKey).GetSchedule(_vlcSourceIds, 180);
+            
 
             return View["Views/Index", schedule];
         }
+
+        private dynamic Play(dynamic parameters) {
+            int channelId = parameters.id;
+            var plId = _playlist.FirstOrDefault(i => i.ChannelNum == channelId);
+            if (plId != null) {
+                using (var wc = new WebClient()) {
+                    wc.Headers[HttpRequestHeader.Authorization] = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(":megaman22"));
+                    wc.DownloadString("http://localhost:8080/requests/status.xml?command=pl_play&id="+plId.ID);
+                }
+            }
+            return new RedirectResponse("../", RedirectResponse.RedirectType.Temporary);
+        }
+
+        private List<VlcPlaylistItem> GetPlaylist() {
+            string xml;
+            using (var wc = new WebClient()) {
+                wc.Headers[HttpRequestHeader.Authorization] = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(":megaman22"));
+                xml = wc.DownloadString("http://localhost:8080/requests/playlist_jstree.xml");
+            }
+            var doc = XDocument.Parse(xml);
+            var playlist = new Dictionary<int, VlcPlaylistItem>();
+            var items = doc.Descendants("item").Where(i => i.Attribute("uri") != null && i.Attribute("uri").Value.StartsWith("http://"));
+            foreach (var item in items) {
+                var plItem = new VlcPlaylistItem {
+                    Uri = item.Attribute("uri").Value,
+                    ID = Convert.ToInt32(item.Attribute("id").Value.Split('_').Last()),
+                    Name = item.Attribute("name").Value,
+                    ChannelNum = Convert.ToInt32(item.Attribute("uri").Value.Split(new[] {"/v"}, StringSplitOptions.None).Last())
+                };
+                if (!playlist.ContainsKey(plItem.ChannelNum)) {
+                    playlist.Add(plItem.ChannelNum, plItem);
+                }
+            }
+
+            return playlist.Values.ToList();
+        }
+
         private dynamic All(dynamic parameters) {
 
             var schedule = new RoviApi(_apiKey).GetSchedule(_channelSourceIds, 180);
@@ -42,5 +88,12 @@ namespace ChannelListings.Modules {
 
             return View["Views/Index", schedule];
         }
+    }
+
+    public class VlcPlaylistItem {
+        public int ID { get; set; }
+        public string Uri { get; set; }
+        public int ChannelNum { get; set; }
+        public string Name { get; set; }
     }
 }
